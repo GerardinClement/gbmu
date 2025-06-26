c53e96d458dee5c6ecf2d57f9eb1841ba68c30c7
LezardSC <lezardsouscultive@gmail.com>
2025-06-26 13:30:45 +0200

    refacto: region.rs

--- src/mmu/region.rs
+++ src/mmu/region.rs
@@ -2,13 +2,11 @@
 #![allow(dead_code)]
 
 #[derive(PartialEq, Eq)]
-pub(crate) enum MemoryRegion {
-    RomBank0,       // 0x000-0x3FFF: read-only
-    RomBank1N,      // 0x4000-0x7FFF
+pub enum MemoryRegion {
+    Rbank,          // 0x000-0x7FFF: read-only
     Vram,           // 0x8000-0x9FFF
     ExternalRam,    // 0xA000-0xBFFF
-    WorkRamBank0,   // 0xC000-0xCFFF
-    WorkRamBank1_7, // 0xD000-0xDFFF
+    Wram,           // 0xC000-0xDFFF
     Echo,           // 0xE000-0xFDFF: mirror of C000-DDFF
     Oam,            // 0xFE00-0xFE9F: Sprite Attribute Table
     Unusable,       // 0xFEA0-0xFEFF
@@ -19,15 +17,12 @@ pub(crate) enum MemoryRegion {
 
 // TODO refacto in order to have multiple structs instead of one
 impl MemoryRegion {
-    // Given any address, pick its region.
-    pub(crate) fn from(addr: u16) -> Self {
+    pub fn from(addr: u16) -> Self {
         match addr {
-            0x0000..=0x3FFF => MemoryRegion::RomBank0,
-            0x4000..=0x7FFF => MemoryRegion::RomBank1N,
+            0x0000..=0x7FFF => MemoryRegion::Rbank,
             0x8000..=0x9FFF => MemoryRegion::Vram,
             0xA000..=0xBFFF => MemoryRegion::ExternalRam,
-            0xC000..=0xCFFF => MemoryRegion::WorkRamBank0,
-            0xD000..=0xDFFF => MemoryRegion::WorkRamBank1_7,
+            0xC000..=0xDFFF => MemoryRegion::Wram,
             0xE000..=0xFDFF => MemoryRegion::Echo,
             0xFE00..=0xFE9F => MemoryRegion::Oam,
             0xFEA0..=0xFEFF => MemoryRegion::Unusable,
@@ -37,14 +32,14 @@ impl MemoryRegion {
         }
     }
 
-    pub(crate) fn is_readable(&self) -> bool {
+    pub fn is_readable(&self) -> bool {
         !matches!(self, MemoryRegion::Unusable)
     }
-    pub(crate) fn is_writable(&self) -> bool {
+    pub fn is_writable(&self) -> bool {
         !matches!(self, MemoryRegion::Unusable)
     }
 
-    pub(crate) fn translate_into_physical_address(&self, addr: u16) -> u16 {
+    pub fn translate_into_physical_address(&self, addr: u16) -> u16 {
         match self {
             MemoryRegion::Echo => addr - 0x2000,
             _ => addr,
