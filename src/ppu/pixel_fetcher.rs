ab5cfafdffb28f06028257c50e32deb6d96efc85
LezardSC <lezardsouscultive@gmail.com>
2026-04-17 19:57:04 +0200

    feat: init tick in fetcher

--- src/ppu/pixel_fetcher.rs
+++ src/ppu/pixel_fetcher.rs
@@ -1,13 +1,19 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
+use crate::mmu::Mmu;
+use crate::mmu::mbc::Mbc;
+use crate::ppu::pixel::Pixel;
+use std::sync::{Arc, RwLock};
+
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Default)]
 pub enum FetcherState {
     #[default]
     GetTileId = 0,
     GetLowData = 1,
     GetHighData = 2,
-    PushPixel = 3,
+    Sleep = 3,
+    PushPixel = 4,
 }
 
 #[derive(Default)]
@@ -18,4 +24,20 @@ pub struct PixelFetcher {
     tile_data_high: u8,
     dot_counter: u32,
     position_counter: u8,
+}
+
+impl PixelFetcher {
+    pub fn tick<T: Mbc>(&mut self, bus: &Arc<RwLock<Mmu<T>>>, ly: u8, scx: u8, scy: u8, use_window: bool) -> Option<[Pixel; 8]> {
+        if self.dot_counter % 2 == 0 {
+            match self.fetcher_state {
+                FetcherState::GetTileId => return None,
+                FetcherState::GetLowData => return None,
+                FetcherState::GetHighData => return None,
+                FetcherState::Sleep => return None,
+                FetcherState::PushPixel => return None,
+            }
+        } else {
+            None
+        }
+    }
 }
