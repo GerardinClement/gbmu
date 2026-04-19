bfeee119f16b5d1b75e41a74c31e4f92a538f364
LezardSC <lezardsouscultive@gmail.com>
2026-04-19 20:56:16 +0200

    feat: pixel struct take oam index and priority

--- src/ppu/pixel.rs
+++ src/ppu/pixel.rs
@@ -8,14 +8,34 @@ pub struct Pixel {
     color: Color,
     is_sprite: bool,
     color_index: u8,
+    priority: bool,
+    oam_index: u8,
 }
 
 impl Pixel {
-    pub fn new(color: Color, is_sprite: bool, color_index: u8) -> Self {
+    pub fn new_bg(color: Color, color_index: u8) -> Self {
+        let priority = false;
+        let oam_index = 0;
+        let is_sprite = false;
+
         Pixel {
             color,
             is_sprite,
             color_index,
+            priority,
+            oam_index,
+        }
+    }
+
+    pub fn new_obj(color: Color, color_index: u8, priority: bool, oam_index: u8) -> Self {
+        let is_sprite = true;
+
+        Pixel {
+            color,
+            is_sprite,
+            color_index,
+            priority,
+            oam_index,
         }
     }
 
@@ -30,6 +50,14 @@ impl Pixel {
     pub fn get_color_index(&self) -> u8 {
         self.color_index
     }
+
+    pub fn get_priority(&self) -> bool {
+        self.priority
+    }
+
+    pub fn get_oam_index(&self) -> u8 {
+        self.oam_index
+    }
 }
 
 impl Default for Pixel {
@@ -38,6 +66,8 @@ impl Default for Pixel {
             color: Color::White,
             is_sprite: false,
             color_index: 0,
+            priority: false,
+            oam_index: 0,
         }
     }
 }
