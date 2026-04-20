b970d2325460e882706f883041724200eeadc289
LezardSC <lezardsouscultive@gmail.com>
2026-04-20 16:26:03 +0200

    feat: Oam_fetcher - get_tile_id

--- src/ppu/oam_fetcher.rs
+++ src/ppu/oam_fetcher.rs
@@ -25,15 +25,17 @@ pub struct OamFetcher {
     tile_data_low: u8,
     tile_data_high: u8,
     dot_counter: u32,
+    actual_sprite_line: usize,
 }
 
 impl OamFetcher {
-     pub fn tick<T: Mbc>(&mut self, bus: &Arc<RwLock<Mmu<T>>>, sprite: &Sprite, piso: &ObjPiso, ly: u8, lcd_control: &LcdControl) -> bool {
+     pub fn tick<T: Mbc>(&mut self, bus: &Arc<RwLock<Mmu<T>>>, sprite: &Sprite, piso: &ObjPiso, ly: u8, lcd_control: &LcdControl, height: u8) -> bool {
         self.dot_counter += 1;
 
         if self.dot_counter % 2 == 0 {
             match self.fetcher_state {
                 FetcherState::GetTileId => {
+                    self.tile_id = self.get_tile_id(bus, sprite, ly, height);
                     self.fetcher_state = FetcherState::GetLowData;
 
                     return false;
@@ -55,5 +57,19 @@ impl OamFetcher {
             }
         }
         false
+     }
+
+     fn get_tile_id<T: Mbc>(&mut self, bus: &Arc<RwLock<Mmu<T>>>, sprite: &Sprite, ly: u8, height: u8) -> u8 {
+        let sprite_top: i16 = sprite.y as i16 - 16;
+        let sprite_line = (ly as i16 - sprite_top) as usize;
+
+        let y_flip = ((sprite.attributes >> 6) & 1) != 0;
+        let actual_sprite_line = if y_flip { (height as usize - 1) - sprite_line } else { sprite_line };
+
+        let tile_always_pair = if height == 16 { sprite.tile & 0xFE } else { sprite.tile };
+        let tile_index = if height == 16 && actual_sprite_line >= 8 { tile_always_pair + 1 } else { tile_always_pair }; // offset if 8x16 because of end of tile index
+
+        self.actual_sprite_line = actual_sprite_line;
+        tile_index
      }
 }
